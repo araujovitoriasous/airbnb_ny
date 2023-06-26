bcf13848d71b69dfbdd0eef28515b9a0eaef0ba7
VitoriaSousa <82541362+araujovitoriasous@users.noreply.github.com>
2023-06-25 22:34:42 -0300

    refac job

--- app.py
+++ app.py
@@ -1,68 +1,74 @@
 from flask import Flask, render_template, request
-from bs4 import BeautifulSoup
 import openai
+import pandas as pd
 
 app = Flask(__name__)
 
-# Configuração da chave de API da OpenAI
+# Configuration of OpenAI API key
 openai.api_key = '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'
 
-# Rota principal
-@app.route('/', methods=['GET', 'POST'])
+# Global variable to store the CSV data
+csv_data = None
+
+# Main route
+@app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/pergunta', methods=['POST', 'GET'])
+def pergunta():
+    global csv_data
+
     if request.method == 'POST':
-        user_input = request.form.get('user_input', '')
+        if csv_data is None:
+            csv_data = scrape_data_from_xlsx()
+            if csv_data is None:
+                return render_template('index.html', how_questions=True, error='Failed to extract data!')
 
-        if user_input.startswith('http'):
-            data = scrape_data_from_file()
-            if data is None:
-                return render_template('index.html', how_questions=True, error='Failed to scrape data from the URL!')
-            return render_template('scraped_data.html', data=data)
+        question = request.form.get('question')
 
-        response = openai.Completion.create(
-            engine="davinci",
-            prompt=user_input,
-            max_tokens=100,
-            n=1,
-            stop=None,
-            temperature=0.7
-        )
-        answer = response.choices[0].text.strip()
-
-        return render_template('index.html', answer=answer, show_questions=True)
+        if question:
+            resposta = responder_pergunta(question, csv_data)
+            return render_template('index.html', answer=resposta, show_questions=True)
 
     return render_template('index.html')
 
+# Perform web scraping from the website
+def scrape_data_from_xlsx():
+    xlsx_filename = 'AB_NYC_2019.xlsx'
+    df = pd.read_excel(xlsx_filename)
+    return df
 
+# Function to answer questions using OpenAI ChatGPT
+def responder_pergunta(pergunta, data):
+    perguntas = [
+        "Qual é o menor valor de aluguel?",
+        "Qual é o maior valor de aluguel?",
+        "Qual é o bairro mais citado?",
+        "Qual é o bairro mais caro?",
+        "Qual é o bairro menos citado?",
+        "Qual é o bairro menos caro?",
+        "Qual é a média de valores?",
+        "Qual é a acomodação mais frequente?"
+    ]
 
-# Realiza o web scraping a partir de um arquivo
-def scrape_data_from_file():
-    try:
-        with open('dados.txt', 'r') as file:
-            arquivo = file.read()
-        soup = BeautifulSoup(arquivo, 'html.parser')
-        return scrape_data(soup)
-    except Exception as e:
-        print(e)
-        return None
+    # Check if the question is in the list of questions
+    if pergunta not in perguntas:
+        return "Desculpe, não posso responder a essa pergunta."
 
-# Realiza o web scraping propriamente dito
-def scrape_data(soup):
-    try:
-        resultados_combinados = soup.find_all("tr", attrs={"class": ["odd", "even"]})
-        data = {}
-        for re in resultados_combinados:
-            td_tags = re.find_all("td")
-            for i, td in enumerate(td_tags):
-                if i not in data:
-                    data[i] = []
-                data[i].append(td.text)
-        return data
-    except Exception as e:
-        print(e)
-        return None
+    # Prepare the prompt for the specific question
+    prompt = f"Pergunta: {pergunta}\nDados: {data}\nResposta:"
 
+    # Generate the response using OpenAI ChatGPT
+    resposta = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        n=1,
+        stop=None,
+        temperature=0.7
+    )
 
+    return resposta.choices[0].text
 
 if __name__ == '__main__':
     app.run(debug=True)
